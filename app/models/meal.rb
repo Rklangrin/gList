283d8aa68bd85f57a705bd24aabd14080d0fa0ea
Rklangrin <rklandgren@gmail.com>
2017-04-03 19:08:54 -0500

    makes minor tweaks

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,7 +2,7 @@ class Meal < ApplicationRecord
   belongs_to :user
   has_many :meal_items
   has_many :ingredients, through: :meal_items
-  accepts_nested_attributes_for :meal_items
+  # accepts_nested_attributes_for :meal_items
 
   def ingredients_array
     arr = []

15db0ed28e3539e4c68e25c623922e394b462883
Rklangrin <rklandgren@gmail.com>
2017-04-20 10:48:49 -0500

    creates list model test, failing right now

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "contains_meal? method" do 
+    let(:list) { List.create(name: "test", user_id: 1)}
+    let(:meal) { Meal.create(name: "meal", user_id: 1)}
+    let(:list_meal) { ListMeal.create(list_id: list.id, meal_id: meal.id )}
+
+    it "returns true if a list already contains a meal" do
+      expect(list.contains_meal?(meal)).to be true
+    end
+  end
 end

677ef6448245469677275c2ac029b06bb9821865
Rklangrin <rklandgren@gmail.com>
2017-05-23 10:47:50 -0500

    adds blue floating element to the card to designate how many times the list has of that meal. Alters List_meals Create.js.erb to reflect an accurate count of how many times a list has one meal

--- app/controllers/list_meals_controller.rb
+++ app/controllers/list_meals_controller.rb
@@ -2,7 +2,8 @@ class ListMealsController < ApplicationController
 
   def create
     @list_meal = ListMeal.find_or_create_by(list_id: params[:list_id], meal_id: params[:id])
-    binding.pry
+    @list = List.find_by(id: params[:list_id])
+
     respond_to do |format|
       format.html { redirect_to :back }
       format.js {}

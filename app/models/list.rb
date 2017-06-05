f80ef94f381247f3cf5e6d427d842471b4aa3875
Rklangrin <rklandgren@gmail.com>
2017-06-05 13:32:00 -0500

    changes lists#number_of method to take meal_id instead of meal object. Alters list_meals#create.js.erb file to use use information from current data as opposed to making a call to the DB.

--- app/models/list.rb
+++ app/models/list.rb
@@ -18,11 +18,11 @@ class List < ApplicationRecord
     contains_meal
   end
 
-  def number_of(meal)
+  def number_of(meal_id)
     list_meals = self.list_meals
     number_of_meals_in_list = 0
     list_meals.each do |lm|
-      if lm.meal_id == meal.id 
+      if lm.meal_id == meal_id
         number_of_meals_in_list += 1
       end
     end
